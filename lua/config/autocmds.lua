9f5b4a7729aa2e460635d3e21492847b6e1c133d
liul85 <liul85@gmail.com>
2023-02-12 12:58:32 +1100

    update plugins

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,24 @@
 -- Add any additional autocmds here
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*/Cargo.toml",
+	group = vim.api.nvim_create_augroup("RustAutoCMDs", { clear = true }),
+	callback = function()
+		local clients = vim.lsp.get_active_clients()
+		local function handler(err)
+			if err then
+				error(tostring(err))
+			end
+			vim.notify("Cargo workspace reloaded")
+		end
+
+		for _, client in ipairs(clients) do
+			if client.name == "rust_analyzer" then
+				vim.notify("Reloading Cargo Workspace")
+				client.request("rust-analyzer/reloadWorkspace", nil, handler, 0)
+			end
+		end
+	end,
+})
